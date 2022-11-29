174b7f42cb0d406c3429a3ac76e85bd41adbfc83
Apple <>
2022-11-29 22:34:59 +0000

    Fixed TimelineView delay when Apple Watch transitions to wrist down.

--- MyWorkouts WatchKit Extension/Views/MetricsView.swift
+++ MyWorkouts WatchKit Extension/Views/MetricsView.swift
@@ -12,9 +12,10 @@ struct MetricsView: View {
     @EnvironmentObject var workoutManager: WorkoutManager
     
     var body: some View {
-        TimelineView(MetricsTimelineSchedule(from: workoutManager.builder?.startDate ?? Date())) { context in
+        TimelineView(MetricsTimelineSchedule(from: workoutManager.builder?.startDate ?? Date(),
+                                             isPaused: workoutManager.session?.state == .paused)) { context in
             VStack(alignment: .leading) {
-                ElapsedTimeView(elapsedTime: workoutManager.builder?.elapsedTime ?? 0, showSubseconds: context.cadence == .live)
+                ElapsedTimeView(elapsedTime: workoutManager.builder?.elapsedTime(at: context.date) ?? 0, showSubseconds: context.cadence == .live)
                     .foregroundStyle(.yellow)
                 Text(Measurement(value: workoutManager.activeEnergy, unit: UnitEnergy.kilocalories)
                         .formatted(.measurement(width: .abbreviated, usage: .workout, numberFormatStyle: .number.precision(.fractionLength(0)))))
@@ -37,13 +38,21 @@ struct MetricsView_Previews: PreviewProvider {
 
 private struct MetricsTimelineSchedule: TimelineSchedule {
     var startDate: Date
+    var isPaused: Bool
 
-    init(from startDate: Date) {
+    init(from startDate: Date, isPaused: Bool) {
         self.startDate = startDate
+        self.isPaused = isPaused
     }
 
-    func entries(from startDate: Date, mode: TimelineScheduleMode) -> PeriodicTimelineSchedule.Entries {
-        PeriodicTimelineSchedule(from: self.startDate, by: (mode == .lowFrequency ? 1.0 : 1.0 / 30.0))
+    func entries(from startDate: Date, mode: TimelineScheduleMode) -> AnyIterator<Date> {
+        var baseSchedule = PeriodicTimelineSchedule(from: self.startDate,
+                                                    by: (mode == .lowFrequency ? 1.0 : 1.0 / 30.0))
             .entries(from: startDate, mode: mode)
+        
+        return AnyIterator<Date> {
+            guard !isPaused else { return nil }
+            return baseSchedule.next()
+        }
     }
 }
